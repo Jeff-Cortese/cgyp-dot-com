62e5abb0cc087f9bd3888955f86bcec6ff2b98ae
Jeff-Cortese <jcortese86@gmail.com>
2018-05-12 14:52:57 -0500

    Hamsters

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,42 @@
 import * as React from 'react';
+import { findDOMNode } from 'react-dom';
 import './App.css';
 
-import logo from './logo.svg';
-
 class App extends React.Component {
+  public componentDidMount() {
+    this.setState({
+      isPlaying: true
+    });
+  }
+
+  public onAppClick() {
+    const dodo: any = findDOMNode(this.refs.dodo);
+    const state: any = this.state;
+    if (state.isPlaying) {
+      dodo.pause();
+    } else {
+      dodo.play();
+    }
+
+    this.setState({
+      isPlaying: !state.isPlaying
+    })
+  }
+
   public render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+      <div className="App" onClick={this.onAppClick.bind(this)}>
+        <h1 className="App-title">Come Get Your Podcast</h1>
+
         <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
+          <img src="hamster2.gif" title="Adam" alt="Adam"/>
+          <img src="hamster3.gif" title="Dustin" alt="Dustin"/>
+          <img src="hamster4.gif" title="Chris" alt="Chris"/>
         </p>
+
+        <audio ref="dodo" autoPlay={true} loop={true}>
+          <source src="dodo.wav" type="audio/wav"/>
+        </audio>
       </div>
     );
   }
